b171b6e9dfe15f405e6ea8986e145717d34e2c8e
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-05-06 12:02:42 +0200

    Initial Commit

---
+++ tasks-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
