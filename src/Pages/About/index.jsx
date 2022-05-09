9d3793a1dd4adcc95e2f067b6d6842b143fa244d
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-05-09 15:03:40 +0200

    Add styling to about page

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,20 +1,61 @@
 import React from "react";
 
+import "./About.css";
+
 function About() {
   return (
-    <div>
-      <h1>Lorem Ipsum</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-        elementum turpis tellus, ut hendrerit magna tempor mollis. Cras suscipit
-        lorem eu posuere pretium. Sed ligula odio, vulputate et sagittis nec,
-        porttitor eu neque. Sed varius ligula erat, non pulvinar augue faucibus
-        interdum. Duis tristique ipsum ut tellus imperdiet varius. Cras nec
-        massa eu sem suscipit pharetra sed eget sapien. Nunc facilisis nibh
-        vitae erat egestas congue. Donec vitae auctor purus. Curabitur nibh
-        sapien, aliquet non luctus fringilla, bibendum tincidunt elit. Ut
-        dapibus est ac dolor tincidunt luctus. Integer ac porttitor leo. Donec
-        nec posuere ipsum, ac sodales ante.
+    <div className="about-div">
+      <h1 className="about-header">Lorem Ipsum</h1>
+      <p className="about-paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        pulvinar sem sed nulla suscipit lacinia. Sed quis mauris nec tortor
+        scelerisque iaculis non elementum leo. Sed maximus ultrices justo. In
+        hac habitasse platea dictumst. Quisque lobortis dictum sem, sit amet
+        semper urna rhoncus et. Praesent finibus ac orci eu tincidunt. Nulla
+        porta mauris eu mauris hendrerit posuere. Nulla facilisi. Integer in
+        nunc sapien. Suspendisse vitae mattis ante, vitae ornare dui. Vivamus
+        cursus laoreet sodales. Pellentesque auctor iaculis pretium. Duis
+        elementum tempor gravida. Ut gravida facilisis nibh sed accumsan. Sed
+        vehicula semper nisl id porta. Pellentesque elementum mauris eros, vel
+        lacinia orci gravida eu. Donec interdum diam vitae tortor rhoncus tempus
+        id volutpat nulla. Maecenas et mi elementum, fermentum orci ut, interdum
+        diam.
+      </p>
+
+      <p className="about-paragraph">
+        Integer posuere urna nec est fringilla, eu auctor dui convallis. Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar sem
+        sed nulla suscipit lacinia. Sed quis mauris nec tortor scelerisque
+        iaculis non elementum leo. Sed maximus ultrices justo. In hac habitasse
+        platea dictumst. Quisque lobortis dictum sem, sit amet semper urna
+        rhoncus et. Praesent finibus ac orci eu tincidunt. Nulla porta mauris eu
+        mauris hendrerit posuere. Nulla facilisi. Integer in nunc sapien.
+        Suspendisse vitae mattis ante, vitae ornare dui. Vivamus cursus laoreet
+        sodales. Pellentesque auctor iaculis pretium. Duis elementum tempor
+        gravida. Ut gravida facilisis nibh sed accumsan. Sed vehicula semper
+        nisl id porta. Pellentesque elementum mauris eros, vel lacinia orci
+        gravida eu. Donec interdum diam vitae tortor rhoncus tempus id volutpat
+        nulla. Maecenas et mi elementum, fermentum orci ut, interdum diam.
+        Praesent feugiat leo dolor, in mattis dolor finibus vitae. Orci varius
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+        mus.
+      </p>
+
+      <p className="about-paragraph">
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Curabitur fermentum nunc nec mi dapibus porttitor.
+        Integer posuere urna nec est fringilla, eu auctor dui convallis. Cras
+        dictum quam eu tellus eleifend, vitae venenatis est elementum. Vivamus
+        quis elit metus. Aenean sit amet neque in magna accumsan vehicula. Ut
+        vel risus convallis, commodo justo convallis, elementum velit. Nam
+        facilisis sagittis felis eget sollicitudin. Sed imperdiet egestas tortor
+        nec accumsan. Vestibulum id arcu sed libero tincidunt tempor. Proin
+        eleifend quam quis mattis eleifend. Vivamus mi lacus, vehicula id erat
+        non, tempus laoreet elit. Proin in massa vel ligula bibendum vulputate
+        sed quis mauris.Cras dictum quam eu tellus eleifend, vitae venenatis est
+        elementum. Vivamus quis elit metus. Aenean sit amet neque in magna
+        accumsan vehicula. Ut vel risus convallis, commodo justo convallis,
+        elementum velit.
       </p>
     </div>
   );
