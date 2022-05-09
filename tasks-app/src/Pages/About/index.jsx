92b6f1d57d97efc8903e53f58adb8bdd3b956998
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-05-09 09:50:34 +0200

    Create about page

--- tasks-app/src/Pages/About/index.jsx
+++ tasks-app/src/Pages/About/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>Lorem Ipsum</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        elementum turpis tellus, ut hendrerit magna tempor mollis. Cras suscipit
+        lorem eu posuere pretium. Sed ligula odio, vulputate et sagittis nec,
+        porttitor eu neque. Sed varius ligula erat, non pulvinar augue faucibus
+        interdum. Duis tristique ipsum ut tellus imperdiet varius. Cras nec
+        massa eu sem suscipit pharetra sed eget sapien. Nunc facilisis nibh
+        vitae erat egestas congue. Donec vitae auctor purus. Curabitur nibh
+        sapien, aliquet non luctus fringilla, bibendum tincidunt elit. Ut
+        dapibus est ac dolor tincidunt luctus. Integer ac porttitor leo. Donec
+        nec posuere ipsum, ac sodales ante.
+      </p>
+    </div>
+  );
 }
 
 export default About;
